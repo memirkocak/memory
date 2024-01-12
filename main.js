b267db5d3c0fd9f4f155b43c00c85c411cea74bb
memirkocak <mekocak@edenschool.fr>
2024-01-12 15:30:03 +0100

    first commit

---
+++ main.js
@@ -0,0 +1,21 @@
+import './style.css'
+
+const cartes = document.querySelectorAll('.card');
+
+let choix1;
+
+let choix2;
+
+for(let i = 0; i<cartes.length; i++){
+  cartes[i].addEventListener('click',()=>{
+    cartes[i].style.backgroundColor = "aqua"
+
+  });
+};
+
+if(){
+
+}
+else if(){
+
+}
