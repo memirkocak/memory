9b0d12f3fef306ed0ce97e37889d228e814f4ffb
memirkocak <mekocak@edenschool.fr>
2024-01-12 16:28:26 +0100

    memory

--- main.js
+++ main.js
@@ -2,15 +2,17 @@ import './style.css'
 
 const cartes = document.querySelectorAll('.card');
 
-let choix1;
-
-let choix2;
+let choix1 = "";
 
 for(let i = 0; i<cartes.length; i++){
   cartes[i].addEventListener('click',()=>{
     cartes[i].style.backgroundColor = "aqua"
-
+    if(choix1 == ""){
+      choix1 = i;
+    } else if(cartes[choix1].textContent == cartes[i].textContent) {
+      console.log('pair');
+    }
+    console.log(choix1);
   });
 };
-
 
