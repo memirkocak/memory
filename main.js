91fe1cf28bbb5d13b0e9ec95bfc36c0ab465773d
memirkocak <mekocak@edenschool.fr>
2024-01-15 17:06:53 +0100

    v3

--- main.js
+++ main.js
@@ -1,29 +1,35 @@
 import './style.css'
 
 const cartes = document.querySelectorAll('.card');
-
+let flag = false;
 let choix1 = "";
 
-for (let i = 0; i <= cartes.length; i++) {
+for (let i = 0; i < cartes.length; i++) {
   cartes[i].addEventListener('click', () => {
-    cartes[i].style.backgroundColor = "aqua";
-    if (choix1 === ""){
-      choix1 = i;
-    } else if (cartes[choix1].getAttribute("data-key") === cartes[i].getAttribute("data-key")) {
-      console.log('pair');
-      cartes[choix1].style.backgroundColor = 'aqua';
-      cartes[i].style.backgroundColor = 'aqua';
-      choix1 = '';
-    }
-    else {
-      setTimeout(() => {
-        cartes[choix1].style.backgroundColor = 'black';
-        cartes[i].style.backgroundColor = 'black';
+    if(flag == false) {
+      cartes[i].style.backgroundColor = "aqua";
+      if (choix1 === ""){
+        choix1 = i;
+      } 
+      if (cartes == style.backgroundColor = 'aqua';){
+        console.log('BRAVO')
+      }
+      else if (cartes[choix1].getAttribute("data-key") === cartes[i].getAttribute("data-key")) {
+        console.log('pair');
+        cartes[choix1].style.backgroundColor = 'aqua';
+        cartes[i].style.backgroundColor = 'aqua';
         choix1 = '';
-      }, 1000);
-      
+      }
+      else {   
+        flag = true;
+        setTimeout(() => {
+          cartes[choix1].style.backgroundColor = 'black';
+          cartes[i].style.backgroundColor = 'black';
+          choix1 = '';
+          flag = false;
+        }, 1000);
+      }
     }
-    console.log(choix1);
   });
 };
 
