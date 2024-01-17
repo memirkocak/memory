9268c9373c7a4e2331f90c3eb038c0415d042d58
memirkocak <mekocak@edenschool.fr>
2024-01-17 15:44:54 +0100

    memory final version

--- main.js
+++ main.js
@@ -3,18 +3,32 @@ import './style.css'
 const cartes = document.querySelectorAll('.card');
 let flag = false;
 let choix1 = "";
+// let lesdivs = document.querySelectorAll('div')
+
+function random(arr){
+  for (let i = arr.length - 1; i > 0; i--) {
+    const j = Math.floor(Math.random() * (i + 1));
+    const temp = arr[i];
+    arr[i] = arr[j];
+    arr[j] = temp;
+  }
+  return arr;
+}
+const stockarr = random([1,1,2,2,3,3,4,4,5,5,6,6,7,7,8,8])
+console.log(stockarr);
+
 
 for (let i = 0; i < cartes.length; i++) {
+  cartes[i].textContent = stockarr[i];
   cartes[i].addEventListener('click', () => {
+    
+    // lesdivs = random;
     if(flag == false) {
       cartes[i].style.backgroundColor = "aqua";
       if (choix1 === ""){
         choix1 = i;
       } 
-      // if (cartes === style.backgroundColor = 'aqua'){
-      //   console.log('BRAVO')
-      // }
-      else if (cartes[choix1].getAttribute("data-key") === cartes[i].getAttribute("data-key")) {
+      else if (cartes[choix1].textContent === cartes[i].textContent) {
         console.log('pair');
         cartes[choix1].style.backgroundColor = 'aqua';
         cartes[i].style.backgroundColor = 'aqua';
@@ -32,4 +46,3 @@ for (let i = 0; i < cartes.length; i++) {
     }
   });
 };
-
